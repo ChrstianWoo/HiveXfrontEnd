65e2997b4637f98f251f67a493f9a5da628bfa60
Soenyoeknoe <andrew.soen-1@student.uts.edu.au>
2023-10-25 01:20:54 +1100

    layer changes

--- src/pages/ProfilePage.jsx
+++ src/pages/ProfilePage.jsx
@@ -106,7 +106,70 @@ export const ProfilePage = () => {
              class="block w-full text-purple-800 text-sm font-semibold rounded-lg hover:bg-gray-100 focus:outline-none focus:shadow-outline focus:bg-gray-100 hover:shadow-xs p-3 my-4 text-center">
               Edit
             </a>
-            {showPopup && (
+            
+        </div>
+        <hr className="border-t-2 my-4" />
+        <div class="bg-white p-3 shadow-lg rounded-sm">
+          <div class="flex items-center space-x-2 font-semibold text-gray-900 leading-8">
+              <span class="tracking-wide">Notification Preference</span>
+          </div>
+          <div class="text-gray-700">
+              <div class="grid md:grid-cols-2 text-sm">
+                <div class="grid grid-cols-2">
+                    <div class="px-4 py-2 font-semibold">Email</div>
+                    <div class="px-4 py-2"><Switch color="amber" defaultChecked /></div>
+                </div>
+                <div class="grid grid-cols-2">
+                    <div class="px-4 py-2 font-semibold">Mobile</div>
+                    <div class="px-4 py-2"><Switch color="amber" defaultChecked /></div>
+                </div>
+                <div class="grid grid-cols-2">
+                    <div class="px-4 py-2 font-semibold">Notification</div>
+                    <div class="px-4 py-2"><Switch color="amber" defaultChecked /></div>
+                </div>
+                <div class="grid grid-cols-2">
+                    <div class="px-4 py-2 font-semibold">Contact No.</div>
+                    <div class="px-4 py-2"><Switch color="amber" defaultChecked /></div>
+                </div>
+              </div>
+          </div>
+        </div>
+        <hr className="border-t-2 my-4" />
+        <div class="bg-white p-3 shadow-lg rounded-sm">
+          <div class="flex items-center justify-center space-x-2 font-semibold text-gray-900 leading-8">
+            <span class="tracking-wide">Update Preference</span>
+          </div>
+          <div class="text-gray-700">
+            <div class="grid md:grid-cols-2 text-sm">
+              <div class="grid grid-cols-2">
+                <div class="px-4 py-2 font-semibold">Email</div>
+                <div class="px-4 py-2">Jane</div>
+              </div>
+              <div class="grid grid-cols-2">
+                <div class="px-4 py-2 font-semibold">Mobile</div>
+                <div class="px-4 py-2">Doe</div>
+              </div>
+            </div>
+          </div>
+        </div>
+        <hr className="border-t-2 my-4" />
+        <div class="bg-white p-3 shadow-lg rounded-sm">
+          <div class="flex items-center justify-center space-x-2 font-semibold text-gray-900 leading-8">
+            <span clas="text-green-500">
+              <svg class="h-5" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24"
+                  stroke="currentColor">
+                <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2"
+                    d="M16 7a4 4 0 11-8 0 4 4 0 018 0zM12 14a7 7 0 00-7 7h14a7 7 0 00-7-7z" />
+              </svg>
+            </span>
+            <span class="tracking-wide">+ INVITE FRIENDS</span>
+          </div>
+          <div class="text-gray-700">
+            <div class="grid md:grid-cols-2 text-sm">
+            </div>
+          </div>
+        </div>
+        {showPopup && (
               <div className="fixed top-0 left-0 w-full h-full flex items-center justify-center">
                 <div className="absolute top-0 left-0 w-full h-full bg-gray-900 opacity-50" onClick={closePopup}></div>
                 <div className="relative bg-white w-full md:w-96 p-6 rounded-lg shadow-md dark:bg-gray-800 dark:border-gray-700">
@@ -173,68 +236,6 @@ export const ProfilePage = () => {
                 </div>
               </div>
             )}
-        </div>
-        <hr className="border-t-2 my-4" />
-        <div class="bg-white p-3 shadow-lg rounded-sm">
-          <div class="flex items-center space-x-2 font-semibold text-gray-900 leading-8">
-              <span class="tracking-wide">Notification Preference</span>
-          </div>
-          <div class="text-gray-700">
-              <div class="grid md:grid-cols-2 text-sm">
-                <div class="grid grid-cols-2">
-                    <div class="px-4 py-2 font-semibold">Email</div>
-                    <div class="px-4 py-2"><Switch color="amber" defaultChecked /></div>
-                </div>
-                <div class="grid grid-cols-2">
-                    <div class="px-4 py-2 font-semibold">Mobile</div>
-                    <div class="px-4 py-2"><Switch color="amber" defaultChecked /></div>
-                </div>
-                <div class="grid grid-cols-2">
-                    <div class="px-4 py-2 font-semibold">Notification</div>
-                    <div class="px-4 py-2"><Switch color="amber" defaultChecked /></div>
-                </div>
-                <div class="grid grid-cols-2">
-                    <div class="px-4 py-2 font-semibold">Contact No.</div>
-                    <div class="px-4 py-2"><Switch color="amber" defaultChecked /></div>
-                </div>
-              </div>
-          </div>
-        </div>
-        <hr className="border-t-2 my-4" />
-        <div class="bg-white p-3 shadow-lg rounded-sm">
-          <div class="flex items-center justify-center space-x-2 font-semibold text-gray-900 leading-8">
-            <span class="tracking-wide">Update Preference</span>
-          </div>
-          <div class="text-gray-700">
-            <div class="grid md:grid-cols-2 text-sm">
-              <div class="grid grid-cols-2">
-                <div class="px-4 py-2 font-semibold">Email</div>
-                <div class="px-4 py-2">Jane</div>
-              </div>
-              <div class="grid grid-cols-2">
-                <div class="px-4 py-2 font-semibold">Mobile</div>
-                <div class="px-4 py-2">Doe</div>
-              </div>
-            </div>
-          </div>
-        </div>
-        <hr className="border-t-2 my-4" />
-        <div class="bg-white p-3 shadow-lg rounded-sm">
-          <div class="flex items-center justify-center space-x-2 font-semibold text-gray-900 leading-8">
-            <span clas="text-green-500">
-              <svg class="h-5" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24"
-                  stroke="currentColor">
-                <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2"
-                    d="M16 7a4 4 0 11-8 0 4 4 0 018 0zM12 14a7 7 0 00-7 7h14a7 7 0 00-7-7z" />
-              </svg>
-            </span>
-            <span class="tracking-wide">+ INVITE FRIENDS</span>
-          </div>
-          <div class="text-gray-700">
-            <div class="grid md:grid-cols-2 text-sm">
-            </div>
-          </div>
-        </div>
       </div>
     </div>
   );
