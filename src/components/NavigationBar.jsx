cb3b43ba102b105d031838b20e8be8f8f5eeebf7
Soenyoeknoe <andrew.soen-1@student.uts.edu.au>
2023-10-25 15:04:47 +1100

    broker to my deals

--- src/components/NavigationBar.jsx
+++ src/components/NavigationBar.jsx
@@ -108,7 +108,7 @@ const NavigationBar = () => {
                       <Menu.Item>
                         {({ active }) => (
                           <a
-                            href="/broker-dashboard"
+                            href="/my-deals"
                             className={classNames(
                               active ? "bg-gray-100" : "",
                               "block px-4 py-2 text-sm text-gray-700 cursor-not-allowed"
