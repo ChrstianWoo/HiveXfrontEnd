87d0d475654eb61eaa16918a42bc20e4d0feb7e5
ChrstianWoo <100246800+ChrstianWoo@users.noreply.github.com>
2023-11-02 22:05:37 +1100

    Update analyticsPage.jsx

--- src/pages/analyticsPage.jsx
+++ src/pages/analyticsPage.jsx
@@ -1,78 +1,48 @@
-import React, { useState, useEffect } from 'react';
-import { Link } from 'react-router-dom'; // Import Link from react-router-dom
+import React from 'react';
 import BroNavbar from '../components/BrokerNavigationBar';
 
 export const AnalyticsPage = () => {
-  
-
   return (
-    <div className="bg-gray-100 h-screen">
+    <div className="bg-gray-100 min-h-screen">
       <BroNavbar />
-      <div className="w-[390px] h-[844px] relative bg-white">
-    <div className="w-[368px] h-[432px] left-[13px] top-[191px] absolute bg-zinc-100" />
-    <div className="w-[191px] h-[71px] left-[14px] top-0 absolute">
-        <div className="w-[191px] h-[71px] left-0 top-0 absolute bg-white" />
-        <div className="w-[18.57px] h-[44.38px] left-[167.12px] top-[13.31px] absolute" />
-        <div className="left-[5.31px] top-[19.97px] absolute text-black text-base font-bold font-['Roboto'] uppercase">HIVE X - Member </div>
+      <div className="max-w-md mx-auto p-4">
+        <div className="text-2xl font-bold mb-4">Analytics</div>
+
+        {/* Sample section with a heading and a graph */}
+        <div className="bg-white rounded shadow p-4 mb-6">
+          <h3 className="text-lg font-bold mb-3">Coupon Usage vs. Span of Validity</h3>
+          <img
+            className="w-full h-auto"
+            src="https://via.placeholder.com/282x152"
+            alt="Graph"
+          />
+        </div>
+
+        {/* Repeated venue section */}
+        {[1, 2, 3].map((_, index) => (
+          <div key={index} className="bg-white rounded shadow mb-6 p-4">
+            <div className="flex items-center mb-2">
+              <div className="bg-zinc-300 rounded-full w-10 h-10 mr-3" />
+              <div className="text-sm font-bold">Name of Venue</div>
+            </div>
+            <div className="flex justify-between items-center">
+              <div className="text-sky-500 text-xs underline cursor-pointer">See reviews</div>
+              <img
+                className="transform rotate-180"
+                src="https://via.placeholder.com/19x15"
+                alt="Arrow"
+              />
+            </div>
+            <div className="text-xs mt-2">
+              <div className="font-bold mb-1">Conditions</div>
+              <p>Entry before 2 pm <br/>Dine in only <br/>Maximum of 3 patrons</p>
+              <div className="font-bold mt-2">Deal</div>
+              <p>20% off pizza</p>
+            </div>
+          </div>
+        ))}
+      </div>
     </div>
-    <div className="w-[108px] h-[25px] left-[190px] top-[16px] absolute">
-        <div className="w-[108px] h-[25px] left-0 top-0 absolute rounded-[5px] border border-black" />
-    </div>
-    <div className="w-[117px] left-[186px] top-[20px] absolute text-center text-black text-xs font-normal font-['Roboto'] leading-none"> Member mode</div>
-    <div className="w-[25px] h-[23px] left-[314px] top-[17px] absolute bg-zinc-100 rounded-[40px]" />
-    <img className="w-[17px] h-4 left-[318px] top-[20px] absolute" src="https://via.placeholder.com/17x16" />
-    <img className="w-6 h-6 left-[428px] top-[334px] absolute" src="https://via.placeholder.com/24x24" />
-    <div className="w-[25px] h-[25px] left-[158px] top-[16px] absolute bg-zinc-300 rounded-full" />
-    <div className="w-[19px] h-2.5 left-[164px] top-[25px] absolute text-black text-[5px] font-normal font-['Roboto']">Points</div>
-    <div className="w-6 h-6 left-[355px] top-[15px] absolute" />
-    <div className="w-[368px] h-[68px] left-[13px] top-[123px] absolute">
-        <div className="w-[368px] h-[68px] left-0 top-0 absolute bg-zinc-300" />
-        <div className="w-10 h-10 left-[12px] top-[15px] absolute bg-white rounded-full" />
-    </div>
-    <div className="w-[99px] h-[26px] left-[81px] top-[141px] absolute text-center text-black text-[11px] font-normal font-['Roboto']">Name of Venue</div>
-    <div className="w-[166px] h-[55px] left-[102px] top-[157px] absolute text-sky-500 text-[10px] font-normal font-['Roboto'] underline">See reviews</div>
-    <img className="w-[19px] h-[15px] left-[367px] top-[161px] absolute origin-top-left -rotate-180" src="https://via.placeholder.com/19x15" />
-    <div className="w-[368px] h-[68px] left-[14px] top-[623px] absolute">
-        <div className="w-[368px] h-[68px] left-0 top-0 absolute bg-zinc-300" />
-        <div className="w-10 h-10 left-[12px] top-[15px] absolute bg-white rounded-full" />
-    </div>
-    <div className="w-[99px] h-[26px] left-[82px] top-[641px] absolute text-center text-black text-[11px] font-normal font-['Roboto']">Name of Venue</div>
-    <div className="w-[166px] h-[55px] left-[103px] top-[657px] absolute text-sky-500 text-[10px] font-normal font-['Roboto'] underline">See reviews</div>
-    <img className="w-[19px] h-[15px] left-[349px] top-[646px] absolute" src="https://via.placeholder.com/19x15" />
-    <div className="w-[368px] h-[68px] left-[14px] top-[691px] absolute">
-        <div className="w-[368px] h-[68px] left-0 top-0 absolute bg-zinc-300" />
-        <div className="w-10 h-[38px] left-[12px] top-[15px] absolute bg-white rounded-full" />
-    </div>
-    <div className="w-[368px] h-[68px] left-[14px] top-[691px] absolute">
-        <div className="w-[368px] h-[68px] left-0 top-0 absolute bg-zinc-300" />
-        <div className="w-10 h-10 left-[12px] top-[15px] absolute bg-white rounded-full" />
-    </div>
-    <div className="w-[99px] h-[26px] left-[82px] top-[709px] absolute text-center text-black text-[11px] font-normal font-['Roboto']">Name of Venue</div>
-    <div className="w-[166px] h-[55px] left-[103px] top-[725px] absolute text-sky-500 text-[10px] font-normal font-['Roboto'] underline">See reviews</div>
-    <img className="w-[19px] h-[15px] left-[349px] top-[714px] absolute" src="https://via.placeholder.com/19x15" />
-    <div className="w-[368px] h-[68px] left-[14px] top-[759px] absolute">
-        <div className="w-[368px] h-[68px] left-0 top-0 absolute bg-zinc-300" />
-        <div className="w-10 h-[38px] left-[12px] top-[15px] absolute bg-white rounded-full" />
-    </div>
-    <div className="w-[368px] h-[68px] left-[14px] top-[759px] absolute">
-        <div className="w-[368px] h-[68px] left-0 top-0 absolute bg-zinc-300" />
-        <div className="w-10 h-10 left-[12px] top-[15px] absolute bg-white rounded-full" />
-    </div>
-    <div className="w-[99px] h-[26px] left-[82px] top-[777px] absolute text-center text-black text-[11px] font-normal font-['Roboto']">Name of Venue</div>
-    <div className="w-[166px] h-[55px] left-[103px] top-[793px] absolute text-sky-500 text-[10px] font-normal font-['Roboto'] underline">See reviews</div>
-    <img className="w-[19px] h-[15px] left-[349px] top-[782px] absolute" src="https://via.placeholder.com/19x15" />
-    <div className="w-[27px] h-8 left-[32px] top-[105px] absolute text-black text-xs font-normal font-['Roboto']">Icon</div>
-    <div className="w-[110px] h-8 left-[97px] top-[105px] absolute text-black text-[11px] font-normal font-['Roboto']">Venue Name</div>
-    <div className="w-[254px] h-[29px] left-[-8px] top-[201px] absolute text-black text-xs font-bold font-['Roboto']">                Conditions</div>
-    <div className="w-[254px] h-[29px] left-[221px] top-[201px] absolute text-black text-xs font-bold font-['Roboto']">Deal</div>
-    <div className="w-[376px] h-[72px] left-[27px] top-[221px] absolute text-black text-xs font-normal font-['Roboto']">Entry before 2 pm <br/>Dine in only <br/>Maximum of 3 patrons </div>
-    <div className="w-[231.90px] h-[29.77px] left-[221px] top-[216px] absolute text-black text-xs font-normal font-['Roboto']">20% off pizza at  ‘ ‘ </div>
-    <div className="w-[109px] h-9 left-[13px] top-[69px] absolute text-black text-2xl font-bold font-['Roboto']">Analytics</div>
-    <img className="w-[282px] h-[152px] left-[52px] top-[290px] absolute" src="https://via.placeholder.com/282x152" />
-    <div className="w-[363px] h-[29px] left-[97px] top-[273px] absolute text-black text-xs font-bold font-['Roboto']">coupon usage vs. span of validity</div>
-</div>
-    </div>
-    
   );
 };
 
