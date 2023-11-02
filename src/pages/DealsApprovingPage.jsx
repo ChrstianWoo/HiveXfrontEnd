64964f0b447ba9f766f545199d3d512f87e7054e
ChrstianWoo <100246800+ChrstianWoo@users.noreply.github.com>
2023-11-02 23:53:38 +1100

    running deals and deals approving

--- src/pages/DealsApprovingPage.jsx
+++ src/pages/DealsApprovingPage.jsx
@@ -1,15 +1,50 @@
-import React, { useState, useEffect } from 'react';
-import { Link } from 'react-router-dom'; // Import Link from react-router-dom
+import React from 'react';
+import { Link } from 'react-router-dom';
 import VenueNavbar from '../components/VenueNavigationBar';
 
 export const DealsApprovingPage = () => {
-  
-
   return (
-    <div className="bg-gray-100 h-screen">
+    <div className="bg-gray-100 min-h-screen flex flex-col">
       <VenueNavbar />
-      <div className="container mt-5">
-        <h1>Deals Approving</h1>
+      <div className="flex-grow container mx-auto bg-white shadow-lg mt-4 mb-4 rounded-lg overflow-hidden">
+        {/* Header with back icon and title */}
+        <div className="p-4 border-b border-gray-200 flex items-center">
+          <Link to="/previous-page" className="inline-block">
+            {/* Replace with actual icon */}
+            <img src="https://via.placeholder.com/20x20" alt="Back" />
+          </Link>
+          <h1 className="font-bold text-xl text-center flex-grow">Deals to Approve</h1>
+        </div>
+
+        {/* Main content */}
+        <div className="p-4">
+          {/* Deal card */}
+          <div className="border border-gray-300 rounded-lg p-4 mb-4">
+            {/* Venue image */}
+            <img src="https://via.placeholder.com/300x150" alt="Venue" className="w-full h-auto rounded-lg mb-3" />
+            {/* Venue name */}
+            <h2 className="text-lg font-bold mb-2">Venue Name</h2>
+            {/* Deal description */}
+            <p className="text-gray-700 mb-3">Discount Up to 50%</p>
+            {/* Date range */}
+            <p className="text-gray-500 mb-4">Start Date - End Date</p>
+            {/* Other details */}
+            <div className="text-sm text-gray-700">
+              <p>Email: venue@example.com</p>
+              <p>Phone: +61 3 1234 5678</p>
+            </div>
+          </div>
+        </div>
+
+        {/* Action Buttons */}
+        <div className="p-4 flex justify-between">
+          <Link to="/go-live" className="bg-black rounded-md text-white text-center px-4 py-2">
+            Go Live
+          </Link>
+          <Link to="/edit-deal" className="bg-black rounded-md text-white text-center px-4 py-2">
+            Edit
+          </Link>
+        </div>
       </div>
     </div>
   );
--- src/pages/RunningDealsPage.jsx
+++ src/pages/RunningDealsPage.jsx
@@ -1,15 +1,29 @@
-import React, { useState, useEffect } from 'react';
-import { Link } from 'react-router-dom'; // Import Link from react-router-dom
+import React from 'react';
+import { Link } from 'react-router-dom';
 import VenueNavbar from '../components/VenueNavigationBar';
 
 export const RunningDealsPage = () => {
-  
-
   return (
     <div className="bg-gray-100 h-screen">
       <VenueNavbar />
-      <div className="container mt-5">
-        <h1>Running Deals</h1>
+      <div className="max-w-xs mx-auto bg-white">
+        
+        <div className="p-4">
+          <div className="text-2xl font-bold mb-2">My Running Deals</div>
+          <div className="bg-zinc-100 p-4 rounded-lg mb-4">
+            <div className="text-center font-bold">Venue name</div>
+            <img className="mx-auto" src="https://via.placeholder.com/227x112" alt="Deal" />
+            <div className="text-xl font-bold mt-2">Coupon Details</div>
+            <div className="text-base font-bold">Discount Up to 50%</div>
+            <div className="text-xs text-center my-2">Valid Until....</div>
+            <div className="font-bold">Restaurant Details</div>
+            <div className="text-[10px]">Phone: +61</div>
+            <div className="text-[10px] mb-4">Email: Meow@gmail.com</div>
+          </div>
+          <div className="flex justify-end">
+            <button className="bg-black text-white rounded px-4 py-2">Pause</button>
+          </div>
+        </div>
       </div>
     </div>
   );
