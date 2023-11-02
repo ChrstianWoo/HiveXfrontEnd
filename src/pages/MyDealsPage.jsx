a6a2fd59ac54861280d3ce73c86d686b57c9810d
ChrstianWoo <100246800+ChrstianWoo@users.noreply.github.com>
2023-11-02 23:41:35 +1100

    remain broker pages

--- src/pages/MyDealsPage.jsx
+++ src/pages/MyDealsPage.jsx
@@ -1,40 +1,35 @@
 import React from 'react';
-import { Link } from 'react-router-dom'; // Import Link from react-router-dom if needed
 import BroNavbar from '../components/BrokerNavigationBar';
 
 export const MyDealsPage = () => {
   return (
-    <div className="bg-gray-100 min-h-screen flex flex-col items-center pt-4">
+    <div className="bg-white">
       <BroNavbar />
-      <div className="flex flex-col items-center px-4 w-full max-w-md mx-auto">
-        <div className="text-center uppercase font-bold text-base mt-4">HIVE X - Broker</div>
-
-        <div className="my-4 p-2 border rounded-lg w-full text-center text-sm">Member Mode</div>
-
-        {/* Rest of the content goes here, adjusted for responsiveness */}
-        {/* For images, ensure you provide alt text for accessibility. */}
-        <img className="w-24 h-24" src="https://via.placeholder.com/150" alt="Placeholder" />
-
-        <div className="text-white bg-black p-4 my-4 w-full">
-          <h2 className="text-2xl mb-2">Venues Trending Right Now.</h2>
-          <p className="text-base">Check out the hottest hospo venues in town.</p>
+      <div className="max-w-sm mx-auto bg-white">
+        
+        
+        <div className="p-4">
+          {/* Placeholder for an image or a graphic representation */}
+          <div className="bg-zinc-300 rounded-full w-10 h-10 mr-4"></div>
+          <div>
+            <div className="text-xs text-black font-bold">Name of Venue</div>
+            <a href="#" className="text-sky-500 text-xs underline">See reviews</a>
+            <div className="flex justify-between items-center">
+              <div className="bg-zinc-100 p-4">
+                <p className="text-xs">Entry before 2 pm<br/>Dine in only<br/>Maximum of 3 patrons</p>
+              </div>
+              <div className="bg-black text-white rounded-full w-10 h-10 flex items-center justify-center">
+                <img src="https://via.placeholder.com/40x40" alt="Status" />
+              </div>
+            </div>
+            <div className="text-xs">
+              <p>20% off pizza at ‘ ‘</p>
+            </div>
+          </div>
         </div>
 
-        <div className="text-white bg-black p-4 my-4 w-full">
-          <h2 className="text-2xl mb-2">Most Claimed Deals.</h2>
-          <p className="text-base">Deals making rounds in the city.</p>
-        </div>
-
-        <div className="bg-white p-4 my-4 w-full">
-          <h2 className="text-2xl text-black">Keys to writing copy that actually converts and sells users</h2>
-          <p className="text-lg mt-2">“The beers are colder at the Maddo” 3.5/5</p>
-          <p className="text-lg text-gray-700 mt-2">Author - Madison Hotel</p>
-        </div>
-
-        {/* Responsive image grid example */}
-        <div className="grid grid-cols-1 md:grid-cols-2 gap-4 w-full">
-          <img className="w-full h-auto" src="https://via.placeholder.com/150" alt="Placeholder" />
-          <img className="w-full h-auto" src="https://via.placeholder.com/150" alt="Placeholder" />
+        <div className="text-center text-2xl font-bold p-4">
+          My Deals
         </div>
       </div>
     </div>
