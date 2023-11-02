edcbf99bd94ec1f143ec45d7b27a37ac0a4c3ad6
ChrstianWoo <100246800+ChrstianWoo@users.noreply.github.com>
2023-11-02 22:15:28 +1100

    Update InvVenuePage.jsx

--- src/pages/InvVenuePage.jsx
+++ src/pages/InvVenuePage.jsx
@@ -1,15 +1,32 @@
-import React, { useState, useEffect } from 'react';
-import { Link } from 'react-router-dom'; // Import Link from react-router-dom
+import React from 'react';
+import { Link } from 'react-router-dom';
 import BroNavbar from '../components/BrokerNavigationBar';
 
 export const InvVenuePage = () => {
-  
-
   return (
     <div className="bg-gray-100 h-screen">
       <BroNavbar />
-      <div className="container mt-5">
-        <h1>Invite Venue</h1>
+      <div className="pt-16 px-4">
+        <div className="text-center text-black text-2xl font-bold mb-6">Invite Venue to HIVE X</div>
+
+         {/* Input fields centered in a column */}
+        <div className="space-y-4 mb-6 w-full max-w-xs px-4">
+          <input type="email" placeholder="Owner Email" className="border border-fuchsia-700 rounded p-2 w-full" />
+          <input type="tel" placeholder="Owner Mobile" className="border border-fuchsia-700 rounded p-2 w-full" />
+          <input type="text" placeholder="Owner Name" className="border border-fuchsia-700 rounded p-2 w-full" />
+          <input type="text" placeholder="Location" className="border border-fuchsia-700 rounded p-2 w-full" />
+          <textarea placeholder="Restaurant Details: Phone: Email:" className="border border-fuchsia-700 rounded p-2 w-full h-24" />
+          <textarea placeholder="Write Description" className="border border-fuchsia-700 rounded p-2 w-full h-24" />
+          <textarea placeholder="HLE Activity" className="border border-fuchsia-700 rounded p-2 w-full h-24" />
+        </div>
+
+        {/* Photo upload section */}
+        <div className="border border-fuchsia-700 rounded p-2 mb-6 text-center text-black text-opacity-50 text-xl">Photo +</div>
+
+        {/* Send Invite button */}
+        <div className="text-center">
+          <button className="bg-black text-white rounded py-2 px-4">Send Invite</button>
+        </div>
       </div>
     </div>
   );
