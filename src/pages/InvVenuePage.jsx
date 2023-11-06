b2c19c2a322b28886af2134f9d4550e416c2a804
Kabilan <kabilan.tiburtius@gmail.com>
2023-11-06 18:05:18 +1100

    broker page bug fixes

--- src/pages/InvVenuePage.jsx
+++ src/pages/InvVenuePage.jsx
@@ -1,55 +1,74 @@
-import React, { useState, useEffect } from 'react';
-import { Link } from 'react-router-dom'; // Import Link from react-router-dom
-import BroNavbar from '../components/BrokerNavigationBar';
-import './InvVenuePage.css'
+import React, { useState, useEffect } from "react";
+import { Link } from "react-router-dom"; // Import Link from react-router-dom
+import BroNavbar from "../components/BrokerNavigationBar";
+import "./InvVenuePage.css";
 export const InvVenuePage = () => {
-  
+  const [hleActivity, setHleActivity] = useState("");
 
+  const handleHleActivityChange = (event) => {
+    setHleActivity(event.target.value);
+  };
   return (
-    <div className="bg-gray-100 h-screen">
-    <BroNavbar />
-    <div className="container mt-5">
-      <h1>Invite Venue to HiveX</h1>
-    </div>
+    <div className="bg-gray-100">
+      <BroNavbar />
+      <div className="container mt-5">
+        <h1>Invite Venue to HiveX</h1>
+      </div>
 
-    <div className="container">
-      <div className="column">
-        <div className="box">
-          <input type="text" placeholder="Owner Email" />
+      <div className="container">
+        <div className="column">
+          <div className="box">
+            <input type="text" placeholder="Owner Email" />
+          </div>
+          <div className="box">
+            <input type="text" placeholder="Owner Mobile" />
+          </div>
+          <div className="box">
+            <input type="text" placeholder="Owner Name" />
+          </div>
+          <div className="box">
+            <input type="text" placeholder="Venue Location" />
+          </div>
         </div>
-        <div className="box">
-          <input type="text" placeholder="Owner Mobile" />
-        </div>
-        <div className="box">
-          <input type="text" placeholder="Owner Name" />
-        </div>
-        <div className="box">
-          <input type="text" placeholder="Owner Location" />
+
+        <div className="column">
+          <div className="box">
+            <input type="text" placeholder="Venue Name" />
+          </div>
+          <div className="box">
+            <input type="text" placeholder="Venue Phone" />
+          </div>
+          <div className="box">
+            <input type="text" placeholder="Venue Email" />
+          </div>
+          <div className="box">
+            <select
+              name="hleActivity"
+              value={hleActivity}
+              id="hleActivity"
+              onChange={handleHleActivityChange}
+              required={true}
+              className="choice-style" 
+            >
+              <option value="">HLE Activity</option>
+              <option value="Hospitality">Hospitality</option>
+              <option value="Entertainment">Entertainment</option>
+              <option value="Leisure">Leisure</option>
+            </select>
+          </div>
+
+          <div className="box">
+            <textarea placeholder="Write Description"></textarea>
+          </div>
         </div>
       </div>
 
-      <div className="column">
-        <div className="box">
-          <input type="text" placeholder="Restaurant Phone" />
-        </div>
-        <div className="box">
-          <input type="text" placeholder="Restaurant Email" />
-        </div>
-        <div className="box">
-          <input type="text" placeholder="Write Description" />
-        </div>
-        <div className="box">
-          <input type="text" placeholder="HLE Activity" />
+      <div className="button_container">
+        <div className="column">
+          <button className="button">Send Invite</button>
         </div>
       </div>
     </div>
-
-    <div className="button_container">
-      <div className="column">
-        <button className="button">Send Invite</button>
-      </div>
-    </div>
-  </div>
   );
 };
 
