4a172cf398519d94c9de9e66978e54fdf1e9aef4
Kabilan <kabilan.tiburtius@gmail.com>
2023-11-06 16:43:45 +1100

    changes

--- src/pages/maps.jsx
+++ src/pages/maps.jsx
@@ -8,13 +8,13 @@ export const LoginPage = (loginPageProps) => {
 
 
   useEffect(() => {
-    // Load the Google Maps API script
+    // (Jack Api)
     const script = document.createElement('script');
     script.src = `https://maps.googleapis.com/maps/api/js?key=AIzaSyDbmf_oibOuPXvtR11eQJiFcvY148s_Aow&callback=initMap&libraries=&v=weekly`;
     script.async = true;
     document.head.appendChild(script);
 
-    // Initialize the map when the script is loaded
+
     window.initMap = () => {
       const sydneyOperaHouse = { lat: -33.8568, lng: 151.2153 };
       const map = new window.google.maps.Map(document.getElementById('map'), {
@@ -23,12 +23,11 @@ export const LoginPage = (loginPageProps) => {
       });
     };
 
-    // Clean up the script tag on component unmount
+    
     return () => {
       document.head.removeChild(script);
     };
-  }, []); // Empty dependency array ensures the effect runs only once
-
+  }, []); 
   return (
     <div className="h-64 w-64 bg-gray-100 flex items-center justify-center">
       <div id="map" className="h-full w-full"></div>
