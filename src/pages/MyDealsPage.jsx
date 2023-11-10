9550827f41a9b7cd3c17421d2a104bb2ad6dd7f3
Kabilan <kabilan.tiburtius@gmail.com>
2023-11-10 15:25:32 +1100

    removed duplicate accordian

--- src/pages/MyDealsPage.jsx
+++ src/pages/MyDealsPage.jsx
@@ -2,10 +2,30 @@ import React, { useState } from 'react';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faChevronDown } from '@fortawesome/free-solid-svg-icons';
 
+
 import BroNavbar from '../components/BrokerNavigationBar';
 
+
+
+const dealsData = [
+  {
+    name: 'Ginger and Spice',
+    image: '/assets/ginger and spice.png',
+    reviewsLink: 'Reviews',
+    conditions: 'Entry before 2 pm\nDine in only\nMaximum of 3 patrons',
+    deal: '20% off pizza',
+  },
+  {
+    name: 'Dominos',
+    image: '/assets/Dominos.png',
+    reviewsLink: 'Reviews',
+    conditions: 'Entry before 2 pm\nDine in only\nMaximum of 3 patrons',
+    deal: '50% off pizza',
+  },
+];
+
 export const MyDealsPage = () => {
-  const [accordionOpen, setAccordionOpen] = useState(Array(3).fill(false));
+  const [accordionOpen, setAccordionOpen] = useState(Array(dealsData.length).fill(false));
 
   const toggleAccordion = (index) => {
     setAccordionOpen((prev) => {
@@ -21,15 +41,14 @@ export const MyDealsPage = () => {
       <div className="max-w-md lg:max-w-4xl mx-auto p-4">
         <div className="text-2xl font-bold mb-4">My Deals</div>
 
-        {[1, 2, 3].map((_, index) => (
+        {dealsData.map((deal, index) => (
           <div key={index} className="bg-white rounded shadow mb-6 p-4">
             <div className="flex items-center mb-2">
-              {/* Placeholder for an image  */}
-              <div className="bg-zinc-300 rounded-full w-10 h-10 mr-4"></div>
-              <div className="text-sm font-bold">Name of Venue</div>
+              <img src={deal.image} alt={deal.name} className="rounded-full w-10 h-10 mr-4" />
+              <div className="text-sm font-bold">{deal.name}</div>
             </div>
             <div className="flex justify-between items-center cursor-pointer">
-              <div className="text-sky-500 text-xs underline">See reviews</div>
+              <div className="text-sky-500 text-xs underline">{deal.reviewsLink}</div>
               <FontAwesomeIcon
                 icon={faChevronDown}
                 className={`w-4 h-4 transition-transform transform ${
@@ -42,12 +61,15 @@ export const MyDealsPage = () => {
             {accordionOpen[index] && (
               <div className="text-xs mt-2">
                 <div className="font-bold mb-1">Conditions</div>
-                <p>Entry before 2 pm <br />Dine in only <br />Maximum of 3 patrons</p>
+         
+                {deal.conditions.split('\n').map((line, lineIndex) => (
+                  <p key={lineIndex}>{line}</p>
+                ))}
                 <div className="flex justify-between items-center mt-2">
                   <div className="font-bold">Deal</div>
                   <div className="text-sky-500 text-xs underline">See Progress</div>
                 </div>
-                <p>20% off pizza</p>
+                <p>{deal.deal}</p>
               </div>
             )}
           </div>
