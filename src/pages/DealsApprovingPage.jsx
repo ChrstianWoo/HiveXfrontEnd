33fea7449524e881301836ead4ca4d6ecaed274f
ChrstianWoo <100246800+ChrstianWoo@users.noreply.github.com>
2023-11-11 21:01:33 +1100

    Update DealsApprovingPage.jsx

--- src/pages/DealsApprovingPage.jsx
+++ src/pages/DealsApprovingPage.jsx
@@ -5,6 +5,7 @@ import DatePicker from 'react-datepicker';
 import 'react-datepicker/dist/react-datepicker.css';
 
 export const DealsApprovingPage = () => {
+  // Initial state setup
   const initialDealsDetails = {
     venueName: 'Adiamo',
     category: 'Mediterranean Food',
@@ -23,6 +24,7 @@ export const DealsApprovingPage = () => {
   const [startDate, setstartDate] = useState(new Date('2024-11-11'));
   const [endDate, setEndDate] = useState(new Date('2024-11-20'));
 
+  // Function definitions
   const openPopup = () => {
     setShowPopup(true);
   };
@@ -49,81 +51,79 @@ export const DealsApprovingPage = () => {
       [name]: value,
     });
   };
-  
-  return (
-    <div className="bg-gray-100 min-h-screen flex flex-col">
-      <VenueNavbar />
-      <div className="flex-grow container mx-auto bg-white shadow-lg mt-4 mb-4 rounded-lg overflow-hidden">
-        {/* Header with back icon and title */}
-        <div className="p-4 border-b border-gray-200 flex items-center">
-          <h1 className="font-bold text-xl text-center flex-grow">Deals to Approve</h1>
-        </div>
 
-        {/* Main content */}
-        <div className="p-4">
-          {/* Deal card */}
-          <div className="border border-gray-300 rounded-lg p-4 mb-4">
-            {/* Venue image */}
-            <img src="assets/Medit-Food.jpg" alt="Venue" className="w-full h-auto rounded-lg mb-3" />
-            {/* Venue name */}
-            <h2 className="text-lg font-bold mb-2 text-purple-700">{deals.venueName}</h2>
-            {/* Deal description */}
-            <p className="text-gray-700 mb-3">{deals.promo}</p>
-            {/* Deal restaurant category */}
-            <p className="text-gray-700 mb-3">{selectedCategory} "{selectedRestriction1} {selectedRestriction2}"</p>
-            {/* Date range */}
-            <p className="text-gray-500 mb-4">{startDate.toLocaleDateString('en-US', { year: 'numeric', month: 'long', day: 'numeric' })} - {endDate.toLocaleDateString('en-US', { year: 'numeric', month: 'long', day: 'numeric' })}</p>
-            {/* Other details */}
-            <div className="text-sm text-gray-700">
-              <p>Email: adiamo@pizza.com</p>
-              <p>Phone: +61 3 1234 5678</p>
+  // JSX for the component
+  return (
+    <div className="bg-gray-100 h-screen">
+      <VenueNavbar />
+      <div className="container mt-5">
+        <div className="flex flex-row min-h-screen justify-center items-center px-6 py-8 mx-auto lg:py-0">
+          <div className="w-full bg-white rounded-lg shadow dark:border md:mt-0 sm:max-w-md xl:p-0 dark:bg-gray-800 dark:border-gray-700">
+            <div className="p-4">
+              <div className="text-2xl font-bold mb-2">Deals to Approve</div>
+              
+              {/* Deal card */}
+              <div className="bg-zinc-100 p-4 rounded-lg mb-4">
+                <div className="text-center font-bold">{deals.venueName}</div>
+                <img className="mx-auto" src="assets/Medit-Food.jpg" alt="Deal" />
+                <div className="text-xl font-bold mt-2">Deal Details</div>
+                <div className="text-base font-bold">{deals.promo}</div>
+                <div className="text-xs text-center my-2">
+                  {startDate.toLocaleDateString('en-US', { year: 'numeric', month: 'long', day: 'numeric' })} - {endDate.toLocaleDateString('en-US', { year: 'numeric', month: 'long', day: 'numeric' })}
+                </div>
+                <div className="font-bold">Restrictions</div>
+                <div className="text-[10px]">Category: {selectedCategory}</div>
+                <div className="text-[10px]">Restriction 1: {selectedRestriction1}</div>
+                <div className="text-[10px] mb-4">Restriction 2: {selectedRestriction2}</div>
+              </div>
+
+              <div className="flex justify-end">
+                <Link to="/go-live" className="bg-black text-white rounded px-4 py-2 mr-2">Go Live</Link>
+                <a onClick={openPopup} className="bg-black text-white rounded px-4 py-2">Edit</a>
+              </div>
             </div>
           </div>
         </div>
+      </div>
 
-        {/* Action Buttons */}
-        <div className="p-4 flex justify-between">
-          <Link to="/go-live" className="bg-black rounded-md text-white text-center px-4 py-2">
-            Go Live
-          </Link>
-          <a onClick={openPopup} className="bg-black rounded-md text-white text-center px-4 py-2">
-            Edit
-          </a>
-        </div>
-        {showPopup && (
-            <div className="fixed top-0 left-0 w-full h-full flex items-center justify-center">
-              <div className="absolute top-0 left-0 w-full h-full bg-gray-900 opacity-50" onClick={closePopup}></div>
-              <div className="relative bg-white w-full md:w-96 p-6 rounded-lg shadow-md dark:bg-gray-800 dark:border-gray-700">
-                <h1 className="text-2xl font-bold text-purple-700 mb-4">Edit Your Profile</h1>
-                <div className="grid grid-cols-2">
-                  <div className="px-4 py-2 font-semibold">Venue Name</div>
-                  <input
-                    type="text"
-                    name="venueName"
-                    value={deals.venueName}
-                    onChange={handleChange}
-                    className="px-4 py-2"
-                  />
-                </div>
-                <div class="grid grid-cols-2">
-                  <div class="px-4 py-2 font-semibold">Promo</div>
-                  <input
-                    type="text"
-                    name="promo"
-                    value={deals.promo}
-                    onChange={handleChange}
-                    className="px-4 py-2"
-                  />
-                </div>
-                <div class="grid grid-cols-2">
-                  <div class="px-4 py-2 font-semibold">Category</div>
-                  <select
-                    name="category"
-                    value={selectedCategory}
-                    onChange={handleCategoryChange}
-                    className="px-4 py-2"
-                  >
-                    <option value="Mediterranean-Food">Mediterranean Food</option>
+      {/* Edit Popup */}
+      {showPopup && (
+        <div className="fixed top-0 left-0 w-full h-full flex items-center justify-center">
+          <div className="absolute top-0 left-0 w-full h-full bg-gray-900 opacity-50" onClick={closePopup}></div>
+          <div className="relative bg-white w-full md:w-1/2 lg:w-1/3 p-6 rounded-lg shadow-md dark:bg-gray-800 dark:border-gray-700">
+            {/* Popup Content */}
+            <h1 className="text-2xl font-bold text-purple-700 mb-4">Edit Deal Details</h1>
+            {/* Form elements for editing deal details */}
+<div className="space-y-4">
+  <div className="grid grid-cols-2 gap-4">
+    <div className="font-semibold">Venue Name</div>
+    <input
+      type="text"
+      name="venueName"
+      value={deals.venueName}
+      onChange={handleChange}
+      className="border rounded px-4 py-2"
+    />
+  </div>
+  <div className="grid grid-cols-2 gap-4">
+    <div className="font-semibold">Promo</div>
+    <input
+      type="text"
+      name="promo"
+      value={deals.promo}
+      onChange={handleChange}
+      className="border rounded px-4 py-2"
+    />
+  </div>
+  <div className="grid grid-cols-2 gap-4">
+    <div className="font-semibold">Category</div>
+    <select
+      name="category"
+      value={selectedCategory}
+      onChange={handleCategoryChange}
+      className="border rounded px-4 py-2"
+    >
+      <option value="Mediterranean-Food">Mediterranean Food</option>
                     <option value="Asian-Food">Asian Food</option>
                     <option value="American-Food">American Food</option>
                     <option value="Fine-Dining"> Fine Dining</option>
@@ -141,34 +141,34 @@ export const DealsApprovingPage = () => {
                     <option value="Music">Music</option>
                     <option value="Movies">Movies</option>
                     <option value="Wellness">Wellness</option>
-                  </select>
-                </div>
-                <div class="grid grid-cols-2">
-                  <div class="px-4 py-2 font-semibold">Restriction</div>
-                  <select
-                    name="category"
-                    value={selectedRestriction1}
-                    onChange={handleRestriction1Change}
-                    className="px-4 py-2"
-                  >
-                    <option value="Vegan">Vegan</option>
+    </select>
+  </div>
+  <div className="grid grid-cols-2 gap-4">
+    <div className="font-semibold">Restriction 1</div>
+    <select
+      name="restriction1"
+      value={selectedRestriction1}
+      onChange={handleRestriction1Change}
+      className="border rounded px-4 py-2"
+    >
+      <option value="Vegan">Vegan</option>
                     <option value="Gluten-Free">Gluten-Free</option>
                     <option value="Dairy-Free">Dairy-Free</option>
                     <option value="Nut-Allergy">Nut-Alleregy</option>
                     <option value="Fish-Allergy">Fish-Alleregy</option>
                     <option value="Halal">Halal</option>
                     <option value="Fun">Fun</option>
-                  </select>
-                </div>
-                <div class="grid grid-cols-2">
-                  <div class="px-4 py-2 font-semibold">Category</div>
-                  <select
-                    name="category"
-                    value={selectedRestriction2}
-                    onChange={handleRestriction2Change}
-                    className="px-4 py-2"
-                  >
-                    <option value="Vegan">Vegan</option>
+    </select>
+  </div>
+  <div className="grid grid-cols-2 gap-4">
+    <div className="font-semibold">Restriction 2</div>
+    <select
+      name="restriction2"
+      value={selectedRestriction2}
+      onChange={handleRestriction2Change}
+      className="border rounded px-4 py-2"
+    >
+      <option value="Vegan">Vegan</option>
                     <option value="Gluten-Free">Gluten-Free</option>
                     <option value="Dairy-Free">Dairy-Free</option>
                     <option value="Nut-Allergy">Nut-Alleregy</option>
@@ -176,49 +176,43 @@ export const DealsApprovingPage = () => {
                     <option value="Halal">Halal</option>
                     <option value="Exciting">Exciting</option>
                     <option value=""></option>
-                  </select>
-                </div>
-                <div class="grid grid-cols-2">
-                    <div class="px-4 py-2 font-semibold">Start Date</div>
-                    <div className="px-4 py-2">
-                      <DatePicker
-                        selected={startDate}
-                        onChange={(date) => setstartDate(date)}
-                        dateFormat="MMMM d, yyyy"
-                      />
-                    </div>
-                </div>
-                <div class="grid grid-cols-2">
-                    <div class="px-4 py-2 font-semibold">End Date</div>
-                    <div className="px-4 py-2">
-                      <DatePicker
-                        selected={endDate}
-                        onChange={(date) => setEndDate(date)}
-                        dateFormat="MMMM d, yyyy"
-                      />
-                    </div>
-                </div>
-                <div className="flex justify-center items-center">
-                  <button
-                    type="button"
-                    onClick={handleSave}
-                    class={`w-full mx-auto text-white bg-gradient-to-r from-purple-500 via-purple-600 to-purple-700 hover:bg-gradient-to-br focus:ring-4 focus:outline-none focus:ring-purple-300 dark:focus:ring-purple-800 shadow-lg shadow-purple-500/50 dark:shadow-lg dark:shadow-purple-800/80 font-medium rounded-lg text-sm px-5 py-2.5 text-center ${
-                      isSaved ? 'bg-green-500' : ''
-                    }`}
-                  >
-                    {isSaved ? 'Saved' : 'Save'}
-                  </button>
-                </div>
-                <button
-                  onClick={closePopup}
-                  className="absolute top-2 right-2 text-purple-700 font-bold text-xl cursor-pointer focus:outline-none"
-                >
-                  &#x2716;
-                </button>
-              </div>
-            </div>
-          )}
-      </div>
+    </select>
+  </div>
+  <div className="grid grid-cols-2 gap-4">
+    <div className="font-semibold">Start Date</div>
+    <DatePicker
+      selected={startDate}
+      onChange={(date) => setstartDate(date)}
+      dateFormat="MMMM d, yyyy"
+      className="border rounded px-4 py-2"
+    />
+  </div>
+  <div className="grid grid-cols-2 gap-4">
+    <div className="font-semibold">End Date</div>
+    <DatePicker
+      selected={endDate}
+      onChange={(date) => setEndDate(date)}
+      dateFormat="MMMM d, yyyy"
+      className="border rounded px-4 py-2"
+    />
+  </div>
+</div>
+
+            <button
+              onClick={handleSave}
+              className={`w-full mx-auto text-white bg-purple-500 hover:bg-purple-700 focus:outline-none focus:ring-4 focus:ring-purple-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center ${isSaved ? 'bg-green-500' : ''}`}
+            >
+              {isSaved ? 'Saved' : 'Save'}
+            </button>
+            <button
+              onClick={closePopup}
+              className="absolute top-2 right-2 text-purple-700 font-bold text-xl cursor-pointer focus:outline-none"
+            >
+              &#x2716;
+            </button>
+          </div>
+        </div>
+      )}
     </div>
   );
 };
