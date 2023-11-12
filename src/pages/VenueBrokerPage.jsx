46488950ac264e8f109c8cd5dad30d98c05e890a
Soenyoeknoe <andrew.soen-1@student.uts.edu.au>
2023-11-12 18:15:21 +1100

    fix

--- src/pages/VenueBrokerPage.jsx
+++ src/pages/VenueBrokerPage.jsx
@@ -55,7 +55,7 @@ export const VenueBrokerPage = () => {
   };
 
   return (
-    <div className="bg-white">
+    <div className="bg-gray-100 min-h-screen">
       <BroNavbar />
       <div className="max-w-md lg:max-w-4xl mx-auto p-4">
         <div className="text-2xl font-bold mb-4">Your Venue</div>
