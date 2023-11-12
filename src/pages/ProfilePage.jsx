7e9442fed456f5d22a42a3a7462d71ddc6b6a5fa
ChrstianWoo <100246800+ChrstianWoo@users.noreply.github.com>
2023-11-12 16:47:15 +1100

    make pop up wider

--- src/pages/ProfilePage.jsx
+++ src/pages/ProfilePage.jsx
@@ -183,7 +183,7 @@ export const ProfilePage = () => {
         {showPopup && (
             <div className="fixed top-0 left-0 w-full h-full flex items-center justify-center">
               <div className="absolute top-0 left-0 w-full h-full bg-gray-900 opacity-50" onClick={closePopup}></div>
-              <div className="relative bg-white w-full md:w-96 p-6 rounded-lg shadow-md dark:bg-gray-800 dark:border-gray-700">
+              <div className="relative bg-white w-full md:w-1/2 lg:w-1/3 p-6 rounded-lg shadow-md dark:bg-gray-800 dark:border-gray-700">
                 <h1 className="text-2xl font-bold text-purple-700 mb-4">Edit Your Profile</h1>
                 <div className="grid grid-cols-2">
                   <div className="px-4 py-2 font-semibold">First Name</div>
