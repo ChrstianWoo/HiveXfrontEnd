b82dbb62bc82e3ef998f9cff134156111a2698c1
Soenyoeknoe <andrew.soen-1@student.uts.edu.au>
2023-11-13 21:13:23 +1100

    star rating

--- src/pages/MyWalletPage.jsx
+++ src/pages/MyWalletPage.jsx
@@ -6,6 +6,7 @@ import { faChevronDown } from '@fortawesome/free-solid-svg-icons';
 const ReviewsPopup = ({ isOpen, onClose }) => {
   const [isSent, setIsSent] = useState(false);
   const [showReviewPopup, setShowReviewPopup] = useState(false);
+  const [selectedStars, setSelectedStars] = useState(0);
   const openReviewPopup = () => {
     setShowReviewPopup(true);
   };
@@ -15,6 +16,9 @@ const ReviewsPopup = ({ isOpen, onClose }) => {
   };
   const handleSent = () => {
     setIsSent(true);
+  };
+  const handleStarClick = (stars) => {
+    setSelectedStars(stars);
   };
   if (!isOpen) return null;
   return (
@@ -72,10 +76,31 @@ const ReviewsPopup = ({ isOpen, onClose }) => {
                   className="absolute top-0 left-0 w-full h-full bg-gray-900 opacity-50"
                   onClick={closeReviewPopup}
                 ></div>
+                
                 <div className="relative bg-white w-full md:w-1/2 lg:w-1/2 p-6 rounded-lg shadow-md dark:bg-gray-800 dark:border-gray-700">
                   <h1 className="text-2xl font-bold text-purple-700 mb-4">
                     Write a Review
                   </h1>
+                  <h3>Rate out of 5 stars</h3>
+                  <div className="flex items-center mb-1 space-x-1 rtl:space-x-reverse">
+                    {[1, 2, 3, 4, 5].map((index) => (
+                      <svg
+                        key={index}
+                        className={`w-4 h-4 ${
+                          index <= selectedStars ? 'text-yellow-300' : 'text-gray-300 dark:text-gray-500'
+                        }`}
+                        aria-hidden="true"
+                        xmlns="http://www.w3.org/2000/svg"
+                        fill="currentColor"
+                        viewBox="0 0 22 20"
+                        onClick={() => handleStarClick(index)}
+                        style={{ cursor: 'pointer' }}
+                      >
+                        <path d="M20.924 7.625a1.523 1.523 0 0 0-1.238-1.044l-5.051-.734-2.259-4.577a1.534 1.534 0 0 0-2.752 0L7.365 5.847l-5.051.734A1.535 1.535 0 0 0 1.463 9.2l3.656 3.563-.863 5.031a1.532 1.532 0 0 0 2.226 1.616L11 17.033l4.518 2.375a1.534 1.534 0 0 0 2.226-1.617l-.863-5.03L20.537 9.2a1.523 1.523 0 0 0 .387-1.575Z" />
+                      </svg>
+                    ))}
+                  </div>
+                  <hr className=" my-4" />
                   <div>
                     <textarea
                     name="venueDescription"
