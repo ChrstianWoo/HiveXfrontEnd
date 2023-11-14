36bf6f921cd970c4ac6a4fe3ebcf7f79a83ea0c5
Kabilan <kabilan.tiburtius@gmail.com>
2023-11-14 16:59:13 +1100

    running project

--- src/pages/Venuecouponaccept.jsx
+++ src/pages/Venuecouponaccept.jsx
@@ -7,20 +7,18 @@ const VenueCouponAccept = () => {
     <div className="bg-gray-100 h-screen">
       <VenueNavbar />
       <div className="flex flex-col items-center justify-center h-screen">
-        <div className="max-w-md p-8 bg-white rounded-lg shadow-lg">
-          <div className="text-2xl font-bold mb-4">Verify Coupon</div>
+        <div className="max-w-md p-8 bg-white rounded-lg shadow-lg text-center">
+          <div className="text-3xl font-bold mb-6">Verify Coupon</div>
 
-     
-          <div>
+          <div className="flex items-center justify-center mb-6">
             <input
               type="text"
               maxLength="4"
-              className="box-input border border-solid border-2 border-purple-700 mb-4"
+              className="box-input text-xl border border-solid border-2 border-purple-700 px-4 py-2 text-center"
             />
           </div>
 
-      
-          <button className="w-full px-6 py-2 bg-purple-700 text-white rounded-md hover:bg-purple-800 focus:outline-none hover:bg-opacity-80">
+          <button className="w-full px-8 py-3 bg-purple-700 text-white rounded-md hover:bg-purple-800 focus:outline-none hover:bg-opacity-80 text-xl">
             Verify
           </button>
         </div>
